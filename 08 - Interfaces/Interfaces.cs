38cb9d58829f3de428846d06a2e1779ec45f89b3
pje279 <p.eckelmann@gmail.com>
2018-04-13 08:02:20 -0500

    Create Interfaces.cs

---
+++ 08 - Interfaces/Interfaces.cs
@@ -0,0 +1,26 @@
+//Define an interface
+interface IEquatable<T>
+{
+    bool Equals(T obj);
+}
+
+//Implement an interface
+public class Car : IEquatable<Car>
+{
+    public string Make {get; set;}
+    public string Model { get; set; }
+    public string Year { get; set; }
+
+    // Implementation of IEquatable<T> interface
+    public bool Equals(Car car)
+    {
+        if (this.Make == car.Make &&
+            this.Model == car.Model &&
+            this.Year == car.Year)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+}
