b6d63f4ce1f77f5ac2bafebe2aae90ff21451bdf
pje279 <p.eckelmann@gmail.com>
2018-04-13 06:56:40 -0500

    Create Types.java

---
+++ 04 - Types/Types.java
@@ -0,0 +1,12 @@
+//Literals
+boolean result = true;
+char capitalC = 'C';
+byte b = 100;
+short s = 10000;
+int i = 100000;
+
+//Floating-Point Literals
+double d1 = 123.4;
+// same value as d1, but in scientific notation
+double d2 = 1.234e2;
+float f1  = 123.4f;
