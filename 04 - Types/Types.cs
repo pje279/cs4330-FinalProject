7ee4d8bd66223245739085f597e6bc0b3f6c7993
pje279 <p.eckelmann@gmail.com>
2018-04-13 06:53:31 -0500

    Create Types.cs

---
+++ 04 - Types/Types.cs
@@ -0,0 +1,9 @@
+// Declaration only
+float temperature;
+string name;
+MyClass myClass;
+
+// Declaration with initializers
+char firstLetter = 'C';
+var limit = 3;
+int[] source = { 0, 1, 2, 3, 4, 5 };
