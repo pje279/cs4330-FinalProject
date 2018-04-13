d8895d1d0557307ac0962b9c3ae68303552775ad
pje279 <p.eckelmann@gmail.com>
2018-04-13 08:06:44 -0500

    Create Interfaces.java

---
+++ 08 - Interfaces/Interfaces.java
@@ -0,0 +1,11 @@
+//Define an interface
+interface printable {  
+  void print();  
+}  
+
+//Implement an interface
+class A6 implements printable {  
+  public void print() {
+  System.out.println("Hello");
+  }
+}
