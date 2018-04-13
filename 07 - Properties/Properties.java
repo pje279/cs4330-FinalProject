553a1354d5a813035a4b6c6658d6b99e8508ff55
pje279 <p.eckelmann@gmail.com>
2018-04-13 07:44:20 -0500

    Create Properties.java

---
+++ 07 - Properties/Properties.java
@@ -0,0 +1,17 @@
+public class Employee {
+  String  name     ;
+  String  position ;
+  int     salary   ;
+  Date    hiredDate;
+  
+  //Getters and setters example
+  public String getName() {
+    return name;
+  }
+  
+  public void  setName(String name) {
+    this.name = name;
+  }
+}
+
+
