56230f8e057393ca2218020325190c1b446f1a8e
pje279 <p.eckelmann@gmail.com>
2018-04-13 07:13:54 -0500

    Create Properties.cs

---
+++ 07 - Properties/Properties.cs
@@ -0,0 +1,52 @@
+//Properties with backing fields
+class TimePeriod
+{
+   private double seconds;
+
+   public double Hours
+   {
+       get { return seconds / 3600; }
+       set { 
+          if (value < 0 || value > 24)
+             throw new ArgumentOutOfRangeException(
+                   $"{nameof(value)} must be between 0 and 24.");
+
+          seconds = value * 3600; 
+       }
+   }
+}
+
+//Expression body definitions
+public class SaleItem
+{
+   string name;
+   decimal cost;
+   
+   public SaleItem(string name, decimal cost)
+   {
+      this.name = name;
+      this.cost = cost;
+   }
+
+   public string Name 
+   {
+      get => name;
+      set => name = value;
+   }
+
+   public decimal Price
+   {
+      get => cost;
+      set => cost = value; 
+   }
+}
+
+//Auto-implemented properties
+public class SaleItem
+{
+   public string Name 
+   { get; set; }
+
+   public decimal Price
+   { get; set; }
+}
