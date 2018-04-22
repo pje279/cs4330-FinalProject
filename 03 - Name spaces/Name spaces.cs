1cf592b13094fd65949f21f254cae3cb259ed17f
pje279 <p.eckelmann@gmail.com>
2018-04-22 17:40:16 -0500

    Update Name spaces.cs

--- 03 - Name spaces/Name spaces.cs
+++ 03 - Name spaces/Name spaces.cs
@@ -1,10 +1,29 @@
 System.Security.Cryptography.AsymmetricAlgorithm aa;
 //may be replaced:
-import System.Security.Crypography; 
-class xxx { ...
+using System.Security.Crypography; 
 AsymmetricAlgorithm aa;
 
-/*There is no alias for packages.
-You have to use import statement 
-or fully-qualified name to 
-mention the specific type. */
+//Alternatively, one could specify an alias 
+//for the the namespace, eg 
+
+using myAlias = System.Security.Crypography; 
+//and then refer to the class with 
+
+myAlias.AsymmetricAlgorithm 
+
+/*****************************************/
+
+namespace N1.N2
+{
+    class A {}
+    class B {}
+}
+//or
+namespace N1
+{
+    namespace N2
+    {
+        class A {}
+        class B {}
+    }
+}
