074ac5e7d08375949769c4e5c2f79d51d6f532c7
pje279 <p.eckelmann@gmail.com>
2018-04-22 17:38:26 -0500

    Update and rename Name spaces-JavaPlaceHolder.txt to Name spaces.java

---
+++ 03 - Name spaces/Name spaces.java
@@ -0,0 +1,25 @@
+System.Security.Cryptography.AsymmetricAlgorithm aa;
+//may be replaced:
+import System.Security.Crypography; 
+class xxx { ...
+AsymmetricAlgorithm aa;
+
+/* There is no alias for packages.
+ * You have to use import statement 
+ * or fully-qualified name to 
+ * mention the specific type. */
+
+pacakge N1.N2;
+    class A {}
+    class B {}
+//or
+package N1.N2;
+   class A {}
+
+//another source file
+package N1.N2;
+   class B {}
+   
+/* package cannot be nested.
+ * One source file can only 
+ * have one package statement. */
