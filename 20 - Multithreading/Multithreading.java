35f0ff8a6cfc458f8ef21a55188d2f5fb9bc1808
pje279 <p.eckelmann@gmail.com>
2018-04-22 17:27:37 -0500

    Update and rename Multithreading-JavaPlaceHolder.txt to Multithreading.java

---
+++ 20 - Multithreading/Multithreading.java
@@ -0,0 +1,25 @@
+//Implementing Runnable
+public class HelloRunnable implements Runnable {
+
+    public void run() {
+        System.out.println("Hello from a thread!");
+    }
+
+    public static void main(String args[]) {
+        (new Thread(new HelloRunnable())).start();
+    }
+
+}
+
+//Subclassing Thread
+public class HelloThread extends Thread {
+
+    public void run() {
+        System.out.println("Hello from a thread!");
+    }
+
+    public static void main(String args[]) {
+        (new HelloThread()).start();
+    }
+
+}
