86c5cf4dc7a0592d64bd4b1888bf835cb080e74a
pje279 <p.eckelmann@gmail.com>
2018-04-22 17:37:09 -0500

    Update and rename Name spaces-C#PlaceHolder.txt to Name spaces.cs

---
+++ 03 - Name spaces/Name spaces.cs
@@ -0,0 +1,10 @@
+System.Security.Cryptography.AsymmetricAlgorithm aa;
+//may be replaced:
+import System.Security.Crypography; 
+class xxx { ...
+AsymmetricAlgorithm aa;
+
+/*There is no alias for packages.
+You have to use import statement 
+or fully-qualified name to 
+mention the specific type. */
